fd4d3090787738004673298bf82b53c420449b48
Martijn Degrève <mardegreve@hotmail.com>
2018-06-16 13:42:22 +0200

    Finished ATM scenario with CashSlot

--- cucumberBook_Example_ATM/features/step_definitions/steps.rb
+++ cucumberBook_Example_ATM/features/step_definitions/steps.rb
@@ -8,15 +8,36 @@ class Account
     end
 end
 class Teller
+    def initialize(cash_slot)
+        @cash_slot = cash_slot
+    end
+
     def withdraw_from(account,amount)
+        @cash_slot.dispense(amount)
     end
 end
-module KnowsMyAccount
+class CashSlot
+    def contents
+        @contents or raise("I'm empty")
+    end
+    
+    def dispense(amount)
+        @contents = amount
+    end
+end    
+module KnowsTheDomain
     def my_account 
         @my_account ||= Account.new
     end
+    def cash_slot
+        @cash_slot ||= CashSlot.new
+    end
+    def teller
+        @Teller ||= Teller.new(cash_slot)
+    end
 end
-World(KnowsMyAccount)
+World(KnowsTheDomain)
+
 module MyCustomHelperModule
     def my_helper_method
     end
@@ -30,9 +51,8 @@ Given("I have deposited ${int} in my account") do |amount|
         "Expected the balance to be #{amount} but it was #{my_account.balance}"
 end
 When("I withdraw ${int}") do |amount|
-    teller = Teller.new
     teller.withdraw_from(my_account, amount)
 end
-Then("${int} should be dispensed") do |int|
-    pending # Write code here that turns the phrase above into concrete actions
+Then("${int} should be dispensed") do |amount|
+    expect(cash_slot.contents).to eq(amount)
 end
